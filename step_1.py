b72b1fb3947e7c7bff46f213c360eb1d66eb502f
Никита <mr.gonk@mail.ru>
2020-12-01 12:33:00 +0500

    initial

---
+++ step_1.py
@@ -0,0 +1,21 @@
+gh = 5
+print(gh, type(gh))
+gh = '5'
+print(gh, type(gh))
+gh = 5.0
+print(gh, type(gh))
+gh = True
+print(gh, type(gh))
+gh = input('Что больше нравится?')
+if gh == 'int':
+    print('Это круто!')
+elif gh == 'float':
+    print('Это замечательно!')
+elif gh == 'str':
+    print('Это хорошо!')
+elif gh == 'bool':
+    print('Это интересно!')
+else:
+    print('Error! 404')
+    print('Boot problem А671Е54')
+print(gh, dir(gh))
